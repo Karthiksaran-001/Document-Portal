7e93e8865b717de8d4e80cb80224a415c6cc5c00
Karthik <mailmekarthik001@gmail.com>
2025-07-28 19:04:19 +0530

    Add requirements

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Setup:
+
+1. Install Environment
+```pip install uv```
+```uv venv --python 3.10```
+
+2. Add Requirements
+```uv pip install -r requirements.txt```
+
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup  , find_packages
 
 setup(
-    name = "documenr_portal",
+    name = "document_portal",
     author= "Karthik Saran",
-    version="0.1",
+    version="0.0.1",
     packages= find_packages(),
     install_requires =[
         "fastapi",
         "uvicorn",
         "python-dotenv"] 
 )
+
+
