2b63bac8e23321d1ddc30acd2ce0c6db9b0c10e0
Karthik <mailmekarthik001@gmail.com>
2025-08-01 11:37:36 +0530

    Code Structure Update

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,6 +1,7 @@
 import logging
 from datetime import datetime
 import os
+import structlog
 
 class CustomLogging:
     def __init__(self , log_dir = "logs"):
@@ -12,9 +13,42 @@ class CustomLogging:
         logging.basicConfig(filename= self.LOG_FILE_PATH,level= logging.INFO,format= "[ %(asctime)s ] %(levelname)s  [%(name)s] - (line-Num:%(lineno)d) -  %(message)s",force= True)
 
     def get_logger(self,name =__file__):
-        return logging.getLogger(name = os.path.basename(name))
+        logger_name = os.path.basename(name)
+        logger = logging.getLogger(logger_name)
+
+        # Configure logging for console + file (both JSON)
+        file_handler = logging.FileHandler(self.LOG_FILE_PATH)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter("%(message)s"))  # Raw JSON lines
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(message)s",  # Structlog will handle JSON rendering
+            handlers=[console_handler, file_handler]
+        )
+
+        # Configure structlog for JSON structured logging
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt="iso", utc=True, key="timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.JSONRenderer()
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use=True,
+        )
+        if not logger.handlers:
+            logger.addHandler(file_handler)
+            logger.addHandler(console_handler)
+        #return structlog.get_logger(logger_name)
+        return logger
 
 if __name__ == "__main__":
     logger = CustomLogging()
     logger = logger.get_logger(__file__)
-    logger.info("Testing")
+    logger.info("Stream Handler is Working")
