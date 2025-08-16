3a16087e5fee3a515ab486a2ba5e92883c5be075
Karthik <mailmekarthik001@gmail.com>
2025-08-16 09:37:50 +0530

    add Base dir in API

--- api/main.py
+++ api/main.py
@@ -29,8 +29,9 @@ app.add_middleware(
 FAISS_BASE = os.getenv("FAISS_BASE", "faiss_index")
 UPLOAD_BASE = os.getenv("UPLOAD_BASE", "data")
 FAISS_INDEX_NAME = os.getenv("FAISS_INDEX_NAME", "index")  # <--- keep consistent with save_local()
-app.mount("/static", StaticFiles(directory="../static"), name="static")
-templates = Jinja2Templates(directory="../templates")
+BASE_DIR = Path(__file__).resolve().parent.parent
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 class FastAPIFileAdapter:
     def __init__(self, uf: UploadFile):
